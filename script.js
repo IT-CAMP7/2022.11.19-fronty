c47153bfd2b03efdbd39bf271e5186969ac65b5f
Mateusz <bmatteo@interia.pl>
2022-11-24 02:45:39 +0100

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+a = 5;
+a = "abc";
+a = 5.5;
+a = 'd';
+a = "d";
+a = 5;
+if(a == 5) {
+	alert('abc');
+} else if(a == 7) {
+} else {
+}
+
+for(i = 5; i < 10; i++) {
+}
+
+console.log("asjdhgfjhasd");
+
+mojaFunkcja();
+
+function mojaFunkcja() {
+	console.log("cos z funkcji !");	
+}
+
+mojaFunkcja();
